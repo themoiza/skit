56f46ea163db8ad924795562714a87b36adda37e
Moisés de Lima <the.moiza@gmail.com>
2022-09-24 17:20:13 -0300

    24/09/2022 - Tooltip

---
+++ README.md
@@ -0,0 +1,7 @@
+# SKIT
+
+SKIT is an advanced front-end for web software development.
+
+The purpose of SKIT is to meet the needs of web software, not for website, blog, or forum, but for use in software.
+
+The goal here is to share the learning for the next generations, learn about native development, without frameworks.
---
+++ src/js/tooltip.js
@@ -0,0 +1,130 @@
+window.Tooltip = {
+
+	handle: (e) => {
+
+		if(!window.lastTooltip){
+			window.lastTooltip = '';
+		}
+		if(!window.toolTips){
+			window.toolTips = {};
+		}
+
+		// CREATE NON HTML TOOLTIP DIV
+		if(e.target && e.target !== document && e.target !== window && typeof(e.target.getAttribute('data-title')) == 'string'){
+
+			if(typeof(e.target.getAttribute('data-title-for')) !== 'string'){
+
+				var randomID = 'tip'+(Math.random()*1000000).toFixed(0);
+
+				var div = document.createElement('div');
+				div.classList.add('tooltip');
+				div.setAttribute('id', randomID);
+				div.textContent = e.target.getAttribute('data-title');
+
+				e.target.setAttribute('data-title-for', randomID);
+				e.target.parentElement.appendChild(div);
+			}
+		}
+
+		// HTML TOOLTIP
+		if(e.target && e.target !== document && e.target !== window && typeof(e.target.getAttribute('data-title-for')) == 'string'){
+
+			var vW = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+			var atual = e.target.getAttribute('data-title-for');
+
+			var tip = document.getElementById(atual);
+
+			var rect = e.target.getBoundingClientRect();
+
+			var position = e.target.getAttribute('data-title-position') ?? 'top';
+
+			var top = (rect.top - tip.clientHeight - 10).toFixed(0);
+			var left = (rect.left+(rect.width / 2)-(tip.clientWidth / 2)).toFixed(0);
+
+			if(top < 5 || position == 'bottom'){
+				top = (rect.top + rect.height + 10).toFixed(0);
+				position = 'bottom';
+
+			}else if(position == 'right'){
+				top = (rect.top + (rect.height / 2) - 10).toFixed(0);
+				left = (rect.left + rect.width + 10).toFixed(0);
+				position = 'right';
+			}else if(position == 'left'){
+				top = (rect.top + (rect.height / 2) - 10).toFixed(0);
+				left = (rect.left - (rect.width * 2) - 10).toFixed(0);
+				position = 'left';
+			}else{
+				var top = (rect.top - tip.clientHeight - 10).toFixed(0);
+				position = 'top';
+			}
+
+			/*var colisionRight = (rect.left+(rect.width / 2) + tip.clientWidth).toFixed(0);
+			if(colisionRight > vW){
+				left = (vW - 40 - tip.clientWidth).toFixed(0);
+			}*/
+
+			tip.classList.remove('tooltip-top');
+			tip.classList.remove('tooltip-left');
+			tip.classList.remove('tooltip-right');
+			tip.classList.remove('tooltip-bottom');
+			tip.classList.add('tooltip-'+position);
+
+			if(tip.getAttribute('data-apply') == null){
+
+				toolTips[atual] = atual;
+				lastTooltip = atual;
+
+				if(left < 5){
+					left = 5;
+				}
+
+				tip.style.top = top+'px';
+				tip.style.left = left+'px';
+
+				tip.setAttribute('data-apply', 'true');
+				tip.classList.add('tooltip-active');
+			}
+		}else{
+
+			lastTooltip = '';
+		}
+
+		// hide tooltips
+		for(var x in toolTips){
+
+			if(document.getElementById(x) && x != lastTooltip){
+
+				var tt = document.getElementById(x);
+				tt.removeAttribute('data-apply');
+				tt.classList.remove('tooltip-active');
+				delete toolTips[x];
+			}
+		}
+	},
+
+	update: (el, t) => {
+
+		if(el){
+
+			if(el.getAttribute('data-title-for') && document.getElementById(el.getAttribute('data-title-for'))){
+
+				var tg = document.getElementById(el.getAttribute('data-title-for'));
+				tg.textContent = t;
+
+				window.setTimeout(() => {
+
+					tg.textContent = el.getAttribute('data-title');
+				}, 1000);
+			}
+		}
+	}
+};
+
+document.addEventListener('mousemove', (e) => {
+	Tooltip.handle(e);
+});
+
+window.addEventListener('scroll', (e) => {
+	Tooltip.handle(e);
+});
