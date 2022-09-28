f7cc1e3df540823b6bd4517f0ed2fc6eca63bb25
Moisés de Lima <the.moiza@gmail.com>
2022-09-27 21:25:39 -0300

    27/09/2022

--- src/js/warning.js
+++ src/js/warning.js
@@ -1,134 +1,160 @@
-window.Warning = {
+window.Warning = (obj) => {
 
-	show: function(obj){
+	/**
+	 * PARAMS
+	 * title: string required
+	 * message: string not required
+	 * color: string not required
+	 * timeout: integer, mili seconds, not required, default 8000
+	 * id: string not required
+	 * position: not required default 8
+	 * - 1: left bottom,
+	 * - 2: center bottom,
+	 * - 3: right bottom,
+	 * - 4: left center,
+	 * - 6: right center,
+	 * - 7: top left,
+	 * - 8: top center, default,
+	 * - 9: top right
+	 */
 
-		if(obj.title){
+	var colors = ['danger', 'pri', 'sec', 'dark', 'light'];
+	var positions = {
+		1: 'Warning-Left-Bottom',
+		2: 'Warning-Center-Bottom',
+		3: 'Warning-Right-Bottom',
+		4: 'Warning-Left-Center',
+		6: 'Warning-Right-Center',
+		7: 'Warning-Left-Top',
+		8: 'Warning-Center-Top',
+		9: 'Warning-Right-Top'
+	};
 
-			var color = '';
-			if(obj.color){
+	var warnLimit = 5;
 
-				if(obj.color == 'danger'){
-					color = 'Warning-danger';
+	if(obj.title && obj.title != ''){
+
+		var title = String(obj.title);
+		var message = String(obj.message ?? '');
+		var color = String(obj.color ?? '');
+		var timeout = obj.timeout ?? 8000;
+
+		// encodeURIComponent is for security, prevent xss
+		var id = encodeURIComponent(String(obj.id ?? 'warning-id-1'));
+		var position = obj.position ?? 8;
+
+		// VALIDATE COLORS
+		if(colors.includes(color)){
+
+			color = 'Warning-'+colors[colors.indexOf(color)];
+
+		// NOT VALID COLORS
+		}else{
+			color = '';
+		}
+
+		// VALIDATE POSITIONS
+		if(positions[position]){
+
+			position = positions[position];
+
+		// NOT VALID COLORS
+		}else{
+			position = positions[8];
+		}
+
+		var warn;
+
+		var mask = `<div style="animation-duration: `+timeout+`ms" class="Warning `+color+`" data-id="`+id+`">
+				<div>
+					<div class="title">`+title+`</div>
+					<div>`+message+`</div>
+				</div>
+				<div><button class="Warning-close">⨉</button></div>
+			</div>`;
+
+		// WARNING EXISTS
+		if(document.getElementById('Warning')){
+
+			warn = document.getElementById('Warning');
+			warn.setAttribute('class', position);
+
+		// WARNING NOT EXISTS
+		}else{
+
+			warn = document.createElement('div');
+			warn.setAttribute('id', 'Warning');
+			warn.setAttribute('class', position);
+			document.body.appendChild(warn);
+
+			warn.addEventListener('click', (e) => {
+
+				var el = e.target;
+				if(e.target.nodeName == 'I'){
+					el = el.parentElement;
 				}
 
-				if(obj.color == 'pri'){
-					color = 'Warning-pri';
+				if(el.nodeName == 'BUTTON'){
+
+					var warnLine = el.parentElement.parentElement;
+
+					var parent = warnLine.parentElement;
+					parent.removeChild(warnLine);
 				}
 
-				if(obj.color == 'sec'){
-					color = 'Warning-sec';
-				}
+			}, true);
+		}
 
-				if(obj.color == 'dark'){
-					color = 'Warning-dark';
-				}
+		/* REMOVE EMPTY CANVAS */
+		var removes = warn.querySelectorAll('.Warning-canvas');
+		removes.forEach(function(el){
 
-				if(obj.color == 'light'){
-					color = 'Warning-light';
-				}
+			if(el.innerHTML == ''){
+				var parent = el.parentElement;
+				parent.removeChild(el);
 			}
+		});
 
-			var warn;
-			var warningDelay = 8000;
-			var id = 'warning';
+		var warnLine = warn.querySelector('div[data-id="'+id+'"]');
 
-			if(obj.timeout){
-				warningDelay = obj.timeout;
-			}
+		var totalCanvas = warn.querySelectorAll('div[data-id]');
 
-			var mask = `<div style="animation-duration: `+warningDelay+`ms" class="Warning `+color+`" data-id="{{id}}">
-					<div>
-						<div class="title">`+obj.title+`</div>
-						<div>`+obj.message+`</div>
-					</div>
-					<div><button class="Warning-close">⨉</button></div>
-				</div>`;
+		// CREATE NEW WARNING
+		if(!warnLine){
 
-			if(obj.id){
-				id = obj.id.replace('"', '');
-			}
+			// LIMIT OF WARNS
+			if(totalCanvas.length < warnLimit){
 
-			mask = mask.split('{{id}}').join(id);
+				var warnLine = document.createElement('div');
+				warnLine.setAttribute('class', 'Warning-canvas');
+				warnLine.innerHTML = mask;
 
-			if(document.getElementById('Warning')){
+				warn.appendChild(warnLine);
 
-				warn = document.getElementById('Warning');
+				Debounce(() => {
 
-			}else{
+					if(typeof(warnLine) !== 'undefined' && typeof(warnLine.parentElement) !== 'undefined'){
 
-				warn = document.createElement('div');
-				warn.setAttribute('id', 'Warning');
-				document.body.appendChild(warn);
+						try{
 
-				warn.addEventListener('click', (e) => {
+							warnLine.parentElement.removeChild(warnLine);
+						}catch{
 
-					var el = e.target;
-					if(e.target.nodeName == 'I'){
-						el = el.parentElement;
-					}
-
-					if(el.nodeName == 'BUTTON'){
-
-						var warnline = el.parentElement.parentElement;
-
-						var parent = warnline.parentElement;
-						parent.removeChild(warnline);
-					}
-
-				}, true);
-			}
-
-			/* REMOVE EMPTY CANVAS */
-			var removes = warn.querySelectorAll('.Warning-canvas');
-			removes.forEach(function(el){
-
-				if(el.innerHTML == ''){
-					var parent = el.parentElement;
-					parent.removeChild(el);
-				}
-			});
-
-			var warnline = warn.querySelector('div[data-id="'+id+'"]');
-
-			var tcanvas = warn.querySelectorAll('div[data-id]');
-
-			if(!warnline){
-
-				// LIMIT OF WARNS
-				if(tcanvas.length < 5){
-
-					var warnline = document.createElement('div');
-					warnline.setAttribute('class', 'Warning-canvas');
-					warnline.innerHTML = mask;
-
-					warn.appendChild(warnline);
-
-					Debounce(() => {
-
-						if(typeof(warnline) !== 'undefined' && typeof(warnline.parentElement) !== 'undefined'){
-
-							try{
-
-								warnline.parentElement.removeChild(warnline);
-							}catch{
-
-							}
 						}
-					}, warningDelay, id);
-				}
-
-			/* UPDATE WARNING */
-			}else if(warnline){
-
-				warnline.setAttribute('class', `Warning `+color);
-
-				warnline.innerHTML = `
-					<div>
-						<div class="title">`+obj.title+`</div>
-						<div>`+obj.message+`</div>
-					</div>
-					<div><button class="Warning-close">⨉</button></div>`;
+					}
+				}, timeout, id);
 			}
+
+		/* UPDATE WARNING */
+		}else if(warnLine){
+
+			warnLine.setAttribute('class', `Warning `+color);
+
+			warnLine.innerHTML = `<div>
+					<div class="title">`+title+`</div>
+					<div>`+message+`</div>
+				</div>
+				<div><button class="Warning-close">⨉</button></div>`;
 		}
 	}
 };
